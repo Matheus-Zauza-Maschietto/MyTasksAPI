20104448577d2fc46f97ade756597104859f2365
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-01-22 13:27:23 -0300

    updae

--- Migrations/20230120162309_creatingFirstMigration.Designer.cs
+++ Migrations/20230120162309_creatingFirstMigration.Designer.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyTasksAPI.Context;
 
+
 #nullable disable
 
 namespace MyTasksAPI.Migrations
--- Services/JwtCodeGenerator.cs
+++ Services/JwtCodeGenerator.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
 
 namespace MyTasksAPI.Services
 {
